50fc173ed406b282f5391ace02ea7d9b95c86113
ilkay <06ilkayozturk@gmail.com>
2023-10-22 22:17:11 +0300

    indexjs

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+/* ----------------------------------------
+    NODEJS EXPRESS | FLİGHT RESERVATİON API
+------------------------------------------- */
+const express = require("express");
+const app = express();
+/* -------------------------------------------------------*/
