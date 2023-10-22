dbb6b0bfe4dcdeea3b6b97363bd96369d727e69d
ilkay <06ilkayozturk@gmail.com>
2023-10-22 22:21:02 +0300

    dbConnection

---
+++ src/configs/dbConnection.js
@@ -0,0 +1,19 @@
+"use strict";
+/* ----------------------------------------
+    NODEJS EXPRESS | FLİGHT RESERVATİON API
+------------------------------------------- */
+// MongoDB Connection:
+const mongoose = require("mongoose");
+
+const dbConnection = function () {
+  // Connect:
+  mongoose
+    .connect(process.env.MONGODB)
+    .then(() => console.log("* DB Connected * "))
+    .catch((err) => console.log("* DB Not Connected * ", err));
+};
+/* -------------------------------------------------------*/
+module.exports = {
+  mongoose,
+  dbConnection,
+};
