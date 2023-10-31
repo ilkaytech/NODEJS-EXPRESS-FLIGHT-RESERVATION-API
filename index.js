e2962b43289b535a05fc151025699be5de041d0b
ilkay <06ilkayozturk@gmail.com>
2023-10-31 23:37:34 +0300

    dotenv

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 /* ----------------------------------------
     NODEJS EXPRESS | FLİGHT RESERVATİON API
 ------------------------------------------- */
+
 const express = require("express");
 const app = express();
+
 /* -------------------------------------------------------*/
+// Required Modules:
+
+// envVariables to process.env:
+require("dotenv").config();
+const PORT = process.env?.PORT || 8000;
+
+//
