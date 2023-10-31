5cd9ad89a35fa5856df7498bcdc6275a4b1af323
ilkay <06ilkayozturk@gmail.com>
2023-10-31 23:45:12 +0300

    indexjs

--- index.js
+++ index.js
@@ -13,4 +13,30 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env?.PORT || 8000;
 
-//
+//asyncErrors to errorHandler:
+require("express-async-errors");
+
+/* -------------------------------------------------------*/
+// Configrations:
+
+// Connect to DB:
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
+
+/* -------------------------------------------------------*/
+// Middlewares:
+
+// Accept JSON:
+app.use(express.json());
+
+// Check Authentication:
+app.use(require("./src/middlewares/authentication"));
+
+// Run Logger:
+app.use(require("./src/middlewares/logger"));
+
+// res.getModelList():
+app.use(require("./src/middlewares/findSearchSortPage"));
+
+/* -------------------------------------------------------*/
+// Routes:
