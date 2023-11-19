3a4b7a03b7b63b746dc918842187a3ac371479f3
ilkay <06ilkayozturk@gmail.com>
2023-11-19 22:18:24 +0300

    feat:setToken created

---
+++ src/helpers/setToken.js
@@ -0,0 +1,28 @@
+"use strict";
+/* ----------------------------------------
+    NODEJS EXPRESS | FLİGHT RESERVATİON API
+------------------------------------------- */
+
+// setToken(userData:object, isRefresh?:boolean):
+
+const jwt = require("jsonwebtoken");
+
+module.exports = function (userData, isRefresh = false) {
+  const data = {
+    access: userData.toJSON(),
+    refresh: { _id: userData._id, password: userData.password },
+    shortExpiresIn: "30m",
+    longExpiresIn: "3d",
+  };
+
+  return {
+    access: jwt.sign(data.access, process.env.ACCESS_KEY, {
+      expiresIn: data.shortExpiresIn,
+    }),
+    refresh: isRefresh
+      ? undefined
+      : jwt.sign(data.refresh, process.env.REFRESH_KEY, {
+          expiresIn: data.longExpiresIn,
+        }),
+  };
+};
