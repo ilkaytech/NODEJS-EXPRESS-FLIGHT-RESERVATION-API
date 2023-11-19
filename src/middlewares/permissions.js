9660c8028872a23074cd6ece11ecf44cdbf9f469
ilkay <06ilkayozturk@gmail.com>
2023-11-19 22:19:49 +0300

    feat:middleware-permissions-created

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -2,3 +2,39 @@
 /* ----------------------------------------
     NODEJS EXPRESS | FLİGHT RESERVATİON API
 ------------------------------------------- */
+// Middleware: permissions
+
+module.exports = {
+  isLogin: (req, res, next) => {
+    if (process.env.NODE_ENV == "development") return next();
+
+    if (req.user) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login.");
+    }
+  },
+
+  isStaffOrAdmin: (req, res, next) => {
+    if (process.env.NODE_ENV == "development") return next();
+
+    if (req.user && (req.user.isStaff || req.user.isAdmin)) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login and to be Staff.");
+    }
+  },
+
+  isAdmin: (req, res, next) => {
+    if (process.env.NODE_ENV == "development") return next();
+
+    if (req.user && req.user.isAdmin) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login and to be Admin.");
+    }
+  },
+};
